be05e6310a8bc8bbe67545d15ca682bb2d20c4e0
fabricio-stecca <119526980+fabricio-stecca@users.noreply.github.com>
2023-01-12 00:36:37 -0300

    att

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
-/*
 
-let zero = document.getElementById(`zero`)
-let um = document.getElementById(`um`)
-let dois = document.getElementById(`dois`)
-let tres = document.getElementById(`tres`)
-let quatro = document.getElementById(`quatro`)
-let cinco = document.getElementById(`cinco`)
-let seis = document.getElementById(`seis`)
-let sete = document.getElementById(`sete`)
-let oito = document.getElementById(`oito`)
-let nove = document.getElementById(`nove`)
-
-*/
 
 function insert(num) {
 
-    document.getElementById(`res`).innerHTML += num
+    let res = document.getElementById(`res`)
+    let ultimoSlot = res.innerHTML[res.innerHTML.length-1]
+
+    if(res.innerHTML == `` && num == `*` || res.innerHTML == `` && num == `/` || res.innerHTML == `+` && num == `*` || res.innerHTML == `+` && num == `/` || res.innerHTML == `-` && num == `*` || res.innerHTML == `-` && num == `/`) {
+        return
+    } else
+    
+    if(ultimoSlot == `+` && num == `+` || ultimoSlot == `+` && num == `-` || ultimoSlot == `+` && num == `*` || ultimoSlot == `+` && num == `/`) {
+        back()
+        res.innerHTML += num
+        
+    }  else if(ultimoSlot == `-` && num == `+` || ultimoSlot == `-` && num == `-` || ultimoSlot == `-` && num == `*` || ultimoSlot == `-` && num == `/`) {
+        back()
+        res.innerHTML += num
+        
+    } else if(ultimoSlot == `*` && num == `+` || ultimoSlot == `*` && num == `-` || ultimoSlot == `*` && num == `*` || ultimoSlot == `*` && num == `/`) {
+        back()
+        res.innerHTML += num
+        
+    } else if(ultimoSlot == `/` && num == `+` || ultimoSlot == `/` && num == `-` || ultimoSlot == `/` && num == `*` || ultimoSlot == `/` && num == `/`) {
+        back()
+        res.innerHTML += num
+        
+    } else {
+
+       res.innerHTML += num
+
+    }
 
 }
 
